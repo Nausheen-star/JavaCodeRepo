471cf8f1ccb52668158d7724fdf4015e4ec1d020
priteshsinghvats <42635143+priteshsinghvats@users.noreply.github.com>
2025-10-02 20:09:13 +0530

    Update CustomerserviceApplicationTests.java

--- CustomerserviceApplicationTests.java
+++ CustomerserviceApplicationTests.java
@@ -4,4 +4,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomerserviceApplicationTests {
+
+   @GetMapping(CUSTOMER_ACCOUNT_DETAILS_END_POINT)
+    private ResponseEntity<CustomerOperationResponse> getIndividualCustomerDetails(@PathVariable("id") String id) throws CustomerNotFoundException {
+        CustomerExtendedDetails customers = customerService.getCustomerCompleteDetails(Integer.parseInt(id));
+        CustomerOperationResponse CustomerDetails = BuilderUtil.responseBuilder(customers, CUSTOMER_DETAILS_FETCHED_SUCCESSFULLY);
+        return new ResponseEntity<>(CustomerDetails, HttpStatus.OK);
+
+    }
+
 }
+
