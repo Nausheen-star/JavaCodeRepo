5f28f9aa3330513cfc6a6752f387292a78c6f1b6
priteshsinghvats <42635143+priteshsinghvats@users.noreply.github.com>
2025-10-02 20:10:10 +0530

    Update CustomerserviceApplicationTests.java

--- CustomerserviceApplicationTests.java
+++ CustomerserviceApplicationTests.java
@@ -4,4 +4,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomerserviceApplicationTests {
+
+
+
+
+
+  public boolean isPalindrome(String test) {
+    if (test == null || test.trim().isEmpty()) {
+        return true;
+    }
+
+    String s = test.toLowerCase();
+    int i = 0, j = s.length() - 1;
+
+    while (i < j) {
+        // skip non-alphanumeric from the left
+        while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+            i++;
+        }
+        // skip non-alphanumeric from the right
+        while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+            j--;
+        }
+
+        if (s.charAt(i) != s.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
 }
